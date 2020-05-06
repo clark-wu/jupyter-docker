551386443c15129895bc464ff6cb95be393b8b30
clark-wu <30484038+clark-wu@users.noreply.github.com>
2020-05-06 22:42:20 +0800

    修改脚本

--- jupyter-lab/build.sh
+++ jupyter-lab/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker build . -f miniconda3-Dockerfile -t clarkwu/miniconda3:latest
+docker push clarkwu/miniconda3:latest
 docker build . -f lab-base-Dockerfile -t clarkwu/jupyter-lab:latest
+docker push clarkwu/jupyter-lab:latest

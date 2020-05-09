281269e7730e5c7a8d784d2b016e8795190554ba
clark-wu <30484038+clark-wu@users.noreply.github.com>
2020-05-09 08:27:59 +0800

    Update build.sh

--- jupyter-lab/build.sh
+++ jupyter-lab/build.sh
@@ -2,4 +2,4 @@
 docker build . -f miniconda3-Dockerfile -t clarkwu/miniconda3:latest
 docker push clarkwu/miniconda3:latest
 docker build . -f lab-base-Dockerfile -t clarkwu/jupyter-lab:latest
-docker push clarkwu/jupyter-lab:latest
+docker push clarkwu/jupyterlab:latest
